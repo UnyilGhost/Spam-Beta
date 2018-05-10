0b6e50a283d72fb83539cb96e4f5af499fa6882d
UnyilGhost <39154186+UnyilGhost@users.noreply.github.com>
2018-05-10 10:02:04 +0000

    Create Spam-Beta.sh

---
+++ Spam-Beta.sh
@@ -0,0 +1,186 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Spam-Beta v2"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: UnyilGhost"
+sleep 1
+echo "\033[32;1m ★contack: unyilghost@gmail.com"
+sleep 1
+echo "\033[34;1m ★PhantomGhost"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "7. Zipay"
+echo "8. Kfc"
+echo "9. Hooq"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Zipay"
+php 7.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Kfc"
+php 8.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Hooq"
+php 9.php
+echo "\033[31;1m tunggu 2 detik untuk kembali"
+
+sleep 2
+
+sh Spam-Beta.sh
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "Spam-Beta"
+echo "\033[31;1mNama tools: Spam-Beta"
+sleep 1
+echo "\033[33;1mKarya: UnyilGhost Decode By Handika Pratama(V2 By UnyilDecode)"
+sleep 1
+echo "\033[32;1mVersi: v2"
+sleep 1
+echo "\033[36;1mTeam: ★PhantomGhost★"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaSecurityLite"
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "UnyilGhost"
+echo "まやちゃん"
+echo "Security Cyber Art"
+echo "PhantomGhost"
+echo "Ponorogo Defacer Team"
+sleep 1
+echo "And All Member Indonesia Security Lite"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
